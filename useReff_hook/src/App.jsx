f7a5268e83fb65a767b427fdc0474631f178c2e6
sachin diwakar <sachindiwakar1802@gmail.com>
2026-03-11 15:58:15 +0530

    new code

--- useReff_hook/src/App.jsx
+++ useReff_hook/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useRef, useEffect } from "react";
 
-const App = () => {
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
   return (
-    <div>App</div>
-  )
+    <>
+      <p>Type in the input field:</p>
+
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
